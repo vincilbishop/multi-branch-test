b361572538807ec65c4acd2fb397b84c5d464110
Vincil Bishop <vincil@skan.io>
2022-10-16 12:23:26 -0500

    [PROJ-003] Updated for more driving methods

--- car.ts
+++ car.ts
@@ -2,5 +2,18 @@ export class Car {
 
     drive() {
         // This is a function that will drive
+        return {}
+    }
+
+    stop() {
+        // This funtion will stop
+    }
+
+    turnLeft() {
+        // This function will turn left
+    }
+
+    turnRight() {
+        // This function will turn right
     }
 }
